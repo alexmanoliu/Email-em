e3250223b98f8c4c9c2506c66c758cd635c1212c
alexmnl88 <alexandru.manoliu@gmail.com>
2018-04-07 09:46:45 +0300

    Users can see survey results

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveryNew</h2>;
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
   componentDidMount() {
@@ -25,7 +25,7 @@ class App extends Component {
           </div>
         </BrowserRouter>
       </div>
-    );
+      );     
   }
 }
 
